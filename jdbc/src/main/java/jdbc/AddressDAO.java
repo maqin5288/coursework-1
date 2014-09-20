b3c588d0855ce23fc949e64d838d5b6495302570
Srikanth Anusuri <srikanth.anusuri@gmail.com>
2014-09-20 15:36:44 -0400

    Added a new method to select the results based on the city.

--- jdbc/src/main/java/jdbc/AddressDAO.java
+++ jdbc/src/main/java/jdbc/AddressDAO.java
@@ -2,39 +2,57 @@ package jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class AddressDAO {
 
 	public void selectAll() throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection connection = null;
-		try {
-			connection = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/coursework", "root", "mint");
-
-			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement
-					.executeQuery("select * from address");
-
-			while (resultSet.next()) {
-				int id = resultSet.getInt("id");
-				String streetAddress = resultSet.getString("street_address");
-				String apartmentNumber = resultSet
-						.getString("apartment_number");
-
-				System.out.print("id: " + id);
-				System.out.print(", streetAddress: " + streetAddress);
-				System.out.print(", apartmentNumber: " + apartmentNumber);
-				System.out.println();
-			}
-		} finally {
-			if (connection != null) {
-				connection.close();
-			}
+		initializeDriver();
+		try (Connection connection = connectToDatabase()){
+			PreparedStatement statement = connection
+					.prepareStatement("select * from address");
+			printResults(statement);
 		}
 	}
-	
+
+	public void selectAllBasedOnCity(String inputCity)
+			throws ClassNotFoundException, SQLException {
+		initializeDriver();
+		try (Connection connection = connectToDatabase()){
+			String queryString = "select * from address where city=?";
+			PreparedStatement statement = connection
+					.prepareStatement(queryString);
+			statement.setString(1, inputCity);
+			printResults(statement);
+		} 
+	}
+
+	private void initializeDriver() throws ClassNotFoundException {
+		Class.forName("com.mysql.jdbc.Driver");
+	}
+
+	private Connection connectToDatabase() throws SQLException {
+		return DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/coursework", "root", "mint");
+	}
+
+	private void printResults(PreparedStatement statement) throws SQLException {
+		ResultSet resultSet = statement.executeQuery();
+
+		while (resultSet.next()) {
+			int id = resultSet.getInt("id");
+			String streetAddress = resultSet.getString("street_address");
+			String apartmentNumber = resultSet.getString("apartment_number");
+			String city = resultSet.getString("city");
+
+			System.out.print("id: " + id);
+			System.out.print(", streetAddress: " + streetAddress);
+			System.out.print(", apartmentNumber: " + apartmentNumber);
+			System.out.print(", city: " + city);
+			System.out.println();
+		}
+	}
+
 }
--- jdbc/src/test/java/jdbc/AddressDAOTest.java
+++ jdbc/src/test/java/jdbc/AddressDAOTest.java
@@ -5,10 +5,17 @@ import org.junit.Test;
 public class AddressDAOTest {
 
 	@Test
-	public void test() throws Exception {
+	public void selectAll() throws Exception {
 		AddressDAO dao = new AddressDAO();
 		dao.selectAll();
 	}
-	
+
+	@Test
+	public void selectAllBasedOnCity() throws Exception {
+		AddressDAO dao = new AddressDAO();
+		dao.selectAllBasedOnCity("Farmington");
+		System.out.println("*****************");
+		dao.selectAllBasedOnCity("Detroit");
+	}
 
 }
